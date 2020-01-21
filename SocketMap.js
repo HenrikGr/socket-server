a8097a1a3795e4399bbf31d5d1de15c54db04eda
Henrik Grönvall <hgc-ab@outlook.com>
2020-01-21 23:28:35 +0100

    Moved SocketMap constructor

--- SocketMap.js
+++ SocketMap.js
@@ -10,10 +10,10 @@
  * the public interface. You could also use ES2019 private fields, but those
  * are not yet widely available as of the time of my writing.
  */
-const map = Symbol('map')
+const map = Symbol("map");
 
 /**
- * SocketMap class to store a map of user id(s) and sockets
+ * SocketMap class to store user id(s) and associated sockets
  *
  * @public
  * @class
@@ -27,75 +27,78 @@ class SocketMap {
      * Map to hold userId and associated sockets
      *
      * @private
-     * @type {Map<any, any>}
+     * @type {Map<string, socket>}
      */
-    this[map] = new Map()
+    this[map] = new Map();
   }
 
   /**
    * Get socket for a user from the map
    *
    * @public
-   * @param userId
-   * @returns {*}
+   * @param {string} userId - id of the user
+   * @returns {socket|undefined}
    */
   getSocket(userId) {
-    console.log('getSocket: ' + userId)
-    return this[map].get(userId)
+    console.log("getSocket: " + userId);
+    return this[map].get(userId);
   }
 
   /**
    * Check if socket exist in the map for the user
    *
-   * @param userId
-   * @returns {boolean | Promise<boolean>}
+   * @param {string} userId - id of the user
+   * @returns {boolean}
    */
   hasSocket(userId) {
-    console.log('hasSocket: ' + userId)
-    return this[map].has(userId)
+    console.log("hasSocket: " + userId);
+    return this[map].has(userId);
   }
 
   /**
    * Store a user's socket in the map
    *
    * @public
-   * @param userId
-   * @param socket
+   * @param {string} userId - id of the user
+   * @param {socket} socket - the associated socket
    */
   setSocket(userId, socket) {
-    console.log('setSocket: ' + userId)
-    this[map].set(userId, socket)
+    console.log("setSocket: " + userId);
+    this[map].set(userId, socket);
   }
 
   /**
    * Delete docket for a user from the map
    *
    * @public
-   * @param userId
+   * @param {string} userId - id of the user
    */
   deleteSocket(userId) {
-    console.log('deleteSocket: ' + userId)
-    this[map].delete(userId)
+    console.log("deleteSocket: " + userId);
+    this[map].delete(userId);
   }
 
   /**
    * Clear the map containing userIds and associated sockets
    */
   deleteAll() {
-    this[map].clear()
+    this[map].clear();
   }
 
   /**
    * The default iterator for the class.
-   * @returns {Iterator} An iterator for the class.
+   *
+   * @interface iterator protocol
+   * @returns {Iterator} The default iterator for the class.
    */
   [Symbol.iterator]() {
     return this.iterator()
   }
 
   /**
-   * Create an iterator that returns each node's data in the list.
-   * @returns {Iterator} An iterator on the list.
+   * Create an iterator that returns each item in the map.
+   *
+   * @returns {Iterator} An iterator on the map.
    */
   *iterator() {
     for (let [key, value] of this[map]) {
@@ -104,4 +107,4 @@ class SocketMap {
   }
 }
 
-module.exports = SocketMap
+module.exports = SocketMap;
