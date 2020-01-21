11757d2b05608667d1d1d5c0627b3312f9309b0f
Henrik Grönvall <hgc-ab@outlook.com>
2020-01-21 23:33:02 +0100

    Initial commit

---
+++ prettier.config.js
@@ -0,0 +1,15 @@
+/*
+ * @prettier
+ * @copyright (c) 2019 - present, HGC-AB
+ * @licence This source code is licensed under the MIT license described and found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+module.exports = {
+  printWidth: 100,
+  singleQuote: true,
+  semi: false,
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  requirePragma: false
+};
