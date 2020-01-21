353968556fba21bcf8845082d123c9f0f622c56e
Henrik Grönvall <hgc-ab@outlook.com>
2020-01-21 01:26:13 +0100

    Initial commit

---
+++ SocketMap.js
@@ -0,0 +1,109 @@
+/**
+ * @prettier
+ * @copyright (c) 2020 - present, HGC-AB
+ * @licence This source code is licensed under the MIT license described and found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+/*
+ * These symbols are used to represent properties that should not be part of
+ * the public interface. You could also use ES2019 private fields, but those
+ * are not yet widely available as of the time of my writing.
+ */
+const map = Symbol('map')
+
+/**
+ * SocketMap class to store a map of user id(s) and sockets
+ *
+ * @public
+ * @class
+ */
+class SocketMap {
+  /**
+   * Create a socket map instance
+   */
+  constructor() {
+    /**
+     * Map to hold userId and their sockets
+     *
+     * @private
+     * @type {Map<any, any>}
+     */
+    this[map] = new Map()
+  }
+
+  /**
+   * Get socket for a user
+   *
+   * @public
+   * @param userId
+   * @returns {*}
+   */
+  getSocket(userId) {
+    console.log('getSocket: Socket retrieved for user: ' + userId)
+    return this[map].get(userId)
+  }
+
+  /**
+   * Store a user's socket
+   *
+   * @public
+   * @param userId
+   * @param socket
+   */
+  setSocket(userId, socket) {
+    console.log('setSocket: Socket set for user: ' + userId)
+    this[map].set(userId, socket)
+  }
+
+  /**
+   * Delete docket for a user
+   *
+   * @public
+   * @param userId
+   */
+  deleteSocket(userId) {
+    console.log('deleteSocket: Socket deleted for user: ' + userId)
+    this[map].delete(userId)
+  }
+
+  sendConnectionConfirmation(userId) {
+    console.log('sendConnectionConfirmation - user: ' + userId)
+    for (let item of this) {
+      const { key, socket } = item
+      if (socket.readyState === 1 && key === userId) {
+        socket.send(`Connection established for user ${key}.`)
+      }
+    }
+  }
+
+  sendMessageConfirmation(userId) {
+    console.log('sendMessageConfirmation - user: ' + userId)
+    for (let item of this) {
+      const { key, socket } = item
+      if (socket.readyState === 1 && key === userId) {
+        socket.send(`Message received from user ${key}`)
+      }
+    }
+  }
+
+  /**
+   * The default iterator for the class.
+   * @returns {Iterator} An iterator for the class.
+   */
+  [Symbol.iterator]() {
+    return this.iterator()
+  }
+
+  /**
+   * Create an iterator that returns each node's data in the list.
+   * @returns {Iterator} An iterator on the list.
+   */
+  *iterator() {
+    for (let [key, value] of this[map]) {
+      yield { key: key, socket: value }
+    }
+  }
+}
+
+module.exports = new SocketMap()
---
+++ server.js
@@ -0,0 +1,146 @@
+/**
+ * @prettier
+ * @copyright (c) 2020 - present, HGC-AB
+ * @licence This source code is licensed under the MIT license described and found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+
+const http = require('http')
+const session = require('express-session')
+const express = require('express')
+const uuid = require('uuid')
+const WebSocket = require('ws')
+const socketMap = require('./SocketMap')
+const cfenv = require('cfenv')
+const appEnv = cfenv.getAppEnv()
+
+// Create our Express application
+const app = express()
+
+// Serve static resources from build directory
+app.use(express.static('./public'))
+
+// We need the same instance of the session parser in express and WebSocket socket-server.
+const sessionParser = session({
+  saveUninitialized: false,
+  secret: '$eCuRiTy',
+  resave: false
+})
+
+/**
+ * Session parser
+ */
+app.use(sessionParser)
+
+// Simulate login
+app.post('/login', function(req, res) {
+  // "Log in" user and set userId to session.
+  const id = uuid.v4()
+  req.session.userId = id
+  console.log(`Updating session for user ${req.session.userId}`)
+  res.send({ result: 'OK', message: 'Session updated' })
+})
+
+/**
+ * Simulate log out
+ */
+app.delete('/logout', function(req, res) {
+  const { session } = req
+  console.log('Destroy the session for user : ', session.userId)
+
+  if (!session.userId) {
+    res.send({ result: 'OK', message: 'user must login first' })
+  } else {
+    console.log('Destroy the socket if exist : ', session)
+    const socket = socketMap.getSocket(session.userId)
+    session.destroy(function() {
+      if (socket) {
+        socket.close()
+      } else {
+        console.log('There was no socket stored to delete')
+      }
+    })
+
+    res.send({ result: 'OK', message: 'Session destroyed' })
+  }
+})
+
+// Create HTTP socket-server by ourselves.
+const server = http.createServer(app)
+
+// Create web socket socket-server
+const wss = new WebSocket.Server({ clientTracking: false, noServer: true })
+
+/**
+ * Handle upgrade event for the http socket-server
+ * If session parsed ok, emit a connection event and handle the rest there.
+ */
+server.on('upgrade', function(req, socket, head) {
+  sessionParser(req, {}, () => {
+    if (!req.session.userId) {
+      console.log('Session did not contain a valid user! - destroying the socket')
+      socket.destroy()
+      return
+    }
+
+    // Call web socket socket-server to handle the upgrade
+    wss.handleUpgrade(req, socket, head, function(ws) {
+      // Emit a connection event
+      wss.emit('connection', ws, req)
+    })
+  })
+})
+
+/**
+ * Handle connection event for the web socket socket-server
+ * Emitted when the handshake is complete. request is the http GET request sent by the client.
+ * Useful for parsing authority headers, cookie headers, and other information.
+ */
+wss.on('connection', function(socket, req) {
+  const userId = req.session.userId
+  console.log('Connection request from user: ' + userId)
+
+  // Store the socket for the userId
+  socketMap.setSocket(userId, socket)
+
+  socketMap.sendConnectionConfirmation(userId)
+
+  /**
+   * Handle incoming web socket messages
+   */
+  socket.on('message', function(message) {
+    socketMap.sendMessageConfirmation(userId)
+  })
+
+  /**
+   * Handle web socket close event
+   */
+  socket.on('close', function(code, reason) {
+    console.log('Socket closed :', code, reason)
+    socketMap.deleteSocket(userId)
+  })
+
+  socket.on('error', function(error) {
+    console.log('Socket closed :', error)
+  })
+})
+
+/**
+ * Emitted when the socket-server closes. This event depends on the 'close' event of HTTP socket-server only
+ * when it is created internally. In all other cases, the event is emitted independently.
+ */
+wss.on('close', function(error) {
+  console.log('Web socket socket-server closed :', error)
+})
+
+/**
+ * Emitted when an error occurs on the underlying socket-server.
+ */
+wss.on('error', function(error) {
+  console.log('Web socket socket-server error :', error)
+})
+
+// Start the http socket-server.
+server.listen(appEnv.port, function() {
+  console.info('socket-server starting on ' + appEnv.url)
+})
